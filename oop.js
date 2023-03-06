7d7ea3104410ed8709187311d20f91d96cdee940
Umair <mrumairhassan@outlook.com>
2023-03-07 01:40:10 +0500

    Add dynamic value using constructor

--- oop.js
+++ oop.js
@@ -1,6 +1,10 @@
 class Human {
     name = "Human";
+    constructor(n) {
+        this.name = n;
+    }
 }
-const user1 = new Human();
+const user1 = new Human("asd"); // new will create method and console value is      Human { name: 'Human' }
 console.log(user1);
+console.log(user1.name);
 export {};
--- oop.ts
+++ oop.ts
@@ -1,8 +1,15 @@
 class Human {
 
     name : string = "Human";
+
+
+    constructor(n: string) {
+
+        this.name = n
+    }
 }
 
-const user1 = new Human();  // new will create method and console value is      Human { name: 'Human' }
+const user1 = new Human( "asd");  // new will create method and console value is      Human { name: 'Human' }
 
 console.log(user1)
+console.log(user1.name)

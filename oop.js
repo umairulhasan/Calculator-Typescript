14bcbb701206ebc84da58cf24dc2a9043b179e9d
Umair <mrumairhassan@outlook.com>
2023-03-07 01:55:59 +0500

    proper example

--- oop.js
+++ oop.js
@@ -1,13 +1,17 @@
-class Human {
-    name;
-    name1;
-    name2;
-    constructor(n) {
-        this.name = n;
-        this.name1 = n;
-        this.name2 = n;
+class MyClass {
+    prop1;
+    prop2;
+    prop3;
+    constructor(val1, val2, val3) {
+        this.prop1 = val1;
+        this.prop2 = val2;
+        this.prop3 = val3;
     }
 }
-const user = new Human("asd"); // new will create method and console value is      Human { name: 'Human' }
-console.log(user);
+// create an instance of MyClass with some values
+const myInstance = new MyClass("hello", 42, true);
+// access the properties of the instance
+console.log(myInstance.prop1); // output: "hello"
+console.log(myInstance.prop2); // output: 42
+console.log(myInstance.prop3); // output: true
 export {};
--- oop.ts
+++ oop.ts
@@ -1,17 +1,19 @@
-class Human {
-
-    name : string ;
-    name1 : string ;
-    name2 : string ;
-
-    constructor(n: string) {
-
-        this.name = n
-        this.name1 = n
-        this.name2 = n
+class MyClass {
+    prop1: string;
+    prop2: number;
+    prop3: boolean;
+  
+    constructor(val1: string, val2: number, val3: boolean) {
+      this.prop1 = val1;
+      this.prop2 = val2;
+      this.prop3 = val3;
     }
-}
-
-const user = new Human( "asd");  // new will create method and console value is      Human { name: 'Human' }
-
-console.log(user)
+  }
+  
+  // create an instance of MyClass with some values
+  const myInstance = new MyClass("hello", 42, true);
+  
+  // access the properties of the instance
+  console.log(myInstance.prop1); // output: "hello"
+  console.log(myInstance.prop2); // output: 42
+  console.log(myInstance.prop3); // output: true

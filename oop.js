dc0a619ce448a728405f3fd9b3554091bc5eef19
Umair <mrumairhassan@outlook.com>
2023-03-07 17:08:51 +0500

    Inheritance added super function pass value to parent constructor

--- oop.js
+++ oop.js
@@ -1,17 +1,14 @@
-class MyClass {
-    prop1;
-    prop2;
-    prop3;
-    constructor(val1, val2, val3) {
-        this.prop1 = val1;
-        this.prop2 = val2;
-        this.prop3 = val3;
+class Base {
+    k = 4;
+    constructor(k) {
     }
 }
-// create an instance of MyClass with some values
-const myInstance = new MyClass("hello", 42, true);
-// access the properties of the instance
-console.log(myInstance.prop1); // output: "hello"
-console.log(myInstance.prop2); // output: 42
-console.log(myInstance.prop3); // output: true
+class Drived extends Base {
+    constructor() {
+        super(30);
+    }
+}
+const yes = new Drived();
+yes.k;
+console.log(yes);
 export {};
--- oop.ts
+++ oop.ts
@@ -1,19 +1,19 @@
-class MyClass {
-    prop1: string;
-    prop2: number;
-    prop3: boolean;
-  
-    constructor(val1: string, val2: number, val3: boolean) {
-      this.prop1 = val1;
-      this.prop2 = val2;
-      this.prop3 = val3;
+class Base {
+
+    k = 4;
+
+    constructor(k: number){
+
     }
-  }
-  
-  // create an instance of MyClass with some values
-  const myInstance = new MyClass("hello", 42, true);
-  
-  // access the properties of the instance
-  console.log(myInstance.prop1); // output: "hello"
-  console.log(myInstance.prop2); // output: 42
-  console.log(myInstance.prop3); // output: true
+}
+class Drived extends Base {
+
+    constructor (){
+        super(30)
+
+    }
+}
+
+const yes = new Drived();
+yes.k
+console.log(yes);
